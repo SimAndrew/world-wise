8b8d1cb2c0545d7da20f3d83aa3c8d1c2134f865
AndSim <siminiucandrew@gmail.com>
2024-02-10 13:45:18 +0200

    feat: (App.jsx) build structure of project ,use react-router-dom Single Page Application for (HomePage.jsx, Pricing.jsx, Product.jsx, PageNotFound.jsx, Login.jsx) Make page navigation (PageNav.jsx) in(AppLayout) display (Sidebar.jsx ,Map.jsx) use CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product.jsx';
+import HomePage from './pages/HomePage.jsx';
+import Pricing from './pages/Pricing.jsx';
+import Login from './pages/Login.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+import AppLayout from './pages/AppLayout.jsx';
 
 export default function App() {
-	return <h1>World Wise App</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="login" element={<Login />} />
+				<Route path="app" element={<AppLayout />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import styles from './Logo.module.css';
+
+function Logo() {
+	return (
+		<img src="public/logo.png" alt="WorldWise logo" className={styles.logo} />
+	);
+}
+
+export default Logo;
