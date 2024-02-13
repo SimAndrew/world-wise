d1fe137b4e9721e6b8c918b4e00b85aa8c17318a
AndSim <siminiucandrew@gmail.com>
2024-02-13 20:31:02 +0200

    feat (Map) with `www.react-leaflet.js.org` based on `www.leafletjs.com/` display Marker, Popup for cities

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,53 @@
+import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useCities } from '../contexts/CitiesContext.jsx';
 import styles from './Map.module.css';
 
 function Map() {
 	const navigate = useNavigate();
+	const { cities } = useCities();
 
+	const [mapPosition, setMapPosition] = useState([40, 0]);
 	const [searchParams, setSearchParams] = useSearchParams();
 
 	const lat = searchParams.get('lat');
 	const lng = searchParams.get('lng');
 
+	const flagemojiToPNG = (flag) => {
+		let countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+			.map((char) => String.fromCharCode(char - 127397).toLowerCase())
+			.join('');
+		return (
+			<img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+		);
+	};
+
 	return (
-		<div className={styles.mapContainer} onClick={() => navigate('form')}>
-			<h1>Map</h1>
-			<h2>
-				Position {lat}, {lng}
-			</h2>
+		<div className={styles.mapContainer}>
+			<MapContainer
+				className={styles.map}
+				center={mapPosition}
+				zoom={13}
+				scrollWheelZoom={true}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+				/>
+
+				{cities.map((city) => (
+					<Marker
+						position={[city.position.lat, city.position.lng]}
+						key={city.id}
+					>
+						<Popup>
+							<span>{flagemojiToPNG(city.emoji)}</span>
+							<span>{city.cityName}</span>
+						</Popup>
+					</Marker>
+				))}
+			</MapContainer>
 		</div>
 	);
 }
