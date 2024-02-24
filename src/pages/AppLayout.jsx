513da0aed8e864b3c2b660aaf13dc4949e5d4f2b
AndSim <siminiucandrew@gmail.com>
2024-02-24 14:02:59 +0200

    feat: add fake authentication (FakeAuthContext.jsx). (Login) login and logout functionality after success login (User.jsx). FAKE_USER is fake login data. (ProtectedRoute.jsx) protect app unauthorized access if user are not log in

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,13 +1,14 @@
 import Sidebar from '../components/Sidebar.jsx';
 import Map from '../components/Map.jsx';
+import User from '../components/User.jsx';
 import styles from './AppLayout.module.css';
 
 export default function AppLayout() {
 	return (
 		<div className={styles.app}>
 			<Sidebar />
-
 			<Map />
+			<User />
 		</div>
 	);
 }
