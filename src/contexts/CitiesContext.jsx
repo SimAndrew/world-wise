b77a8cddd217fe6975128a21c9991a75534fe246
AndSim <siminiucandrew@gmail.com>
2024-03-02 19:19:23 +0200

    Revert "feat: deploy (cities.json) www.github.com/typicode/json-server on https://my-json-server.typicode.com/SimAndrew/world-wise/data/cities.json"
    This reverts commit bc5c2f996e9924851af1cbfb9fed4cb8ee18cb24.

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
 	useReducer,
 } from 'react';
 
-const BASE_URL = 'https://my-json-server.typicode.com/SimAndrew/world-wise/cities';
+const BASE_URL = 'http://localhost:8000';
 
 const CitiesContext = createContext();
 
